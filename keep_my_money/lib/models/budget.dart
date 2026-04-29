131f5fff8905c0f40f0b3282b40abcd0785f2570
yeji <hallo27@naver.com>
2026-04-29 18:37:18 +0900

    feat: BudgetNotifier 정의

--- keep_my_money/lib/models/budget.dart
+++ keep_my_money/lib/models/budget.dart
@@ -6,4 +6,21 @@ class Budget {
     required this.totalAmount,
     required this.month,
   });
+
+  Budget copyWith({
+    int? totalAmount,
+    String? month
+  }) {
+    return Budget(
+      totalAmount: totalAmount ?? this.totalAmount, 
+      month: month ?? this.month
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'totalAmount': totalAmount,
+      'month': month,
+    };
+  }
 }
---
+++ keep_my_money/lib/providers/budget_notifier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:keep_my_money/models/budget.dart';
+
+class BudgetNotifier extends Notifier<Budget> {
+
+  @override
+  Budget build() {
+    final now = DateTime.now();
+    return Budget(
+      totalAmount: 500000, 
+      month: now.month.toString(),
+    );
+  }
+
+  void updateAmount(int newAmount) {
+    state = state.copyWith(totalAmount: newAmount);
+  }
+
+  final budgetProvider = NotifierProvider<BudgetNotifier, Budget>(() => BudgetNotifier());
+}
